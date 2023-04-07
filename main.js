da177c9ba9617002492177ac7bbb3a3db3d7e4ea
bianca <bianca.g.buha@gmail.com>
2023-04-07 19:07:41 +0100

    kata function created and tests completed

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 /* 
 👉 Write your kata here!
-*/
+
+   Write a function that takes as argument an array of integers,
+   and returns another array with the integers summed up, two by two.
+
+   If the array is empty, return an empty array.
+   If the array has only one integer, return the same array.
+   If the array has an even number of integers (e.g. [1, 2, 3, 4]), return the sum of each pair of integers (e.g. [3, 7]).
+   If the array has an odd number of integers (e.g. [1, 2, 3, 4, 5]), return the sum of each pair of integers, and last integer not summed up (e.g. [3, 7, 5]).
+
+   Example  (Input => Output)
+    [] => []
+    [1] => [1]
+    [1, 2, 3, 4] => [3, 7]
+    [1, 2, 3, 4, 5] => [3, 7, 5]
+
+*/ 
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function modifyArrayOfNums(arrayOfNums) {
+    // write your code here
+    let newArr=[]
+    // if(arrayOfNums.length%2==1) {arrayOfNums.push(0)} // alternative solution to line 28
+    for  (let i=0; i<(arrayOfNums.length-1); i+=2) {
+        newArr.push(arrayOfNums[i]+arrayOfNums[i+1]);
+    }
+    if (arrayOfNums.length%2===1) {
+        newArr.push(arrayOfNums[arrayOfNums.length-1]);
+    } 
+   return newArr;
+};
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,32 @@
 //👉 Write your tests below here:
+import { test, expect } from '@jest/globals';
+import { modifyArrayOfNums } from './main.js';
+
+
+test("modifyArrayOfNums should return an empty array", () => {
+    expect(modifyArrayOfNums([])).toStrictEqual([]);
+})
+
+test("modifyArrayOfNums should return the same array if the array has only one element", () => {
+    expect(modifyArrayOfNums([1545])).toStrictEqual([1545]);
+});
+
+test("modifyArrayOfNums should return an array containing the sum of the 2 numbers", () => {
+    expect(modifyArrayOfNums([58, 2])).toStrictEqual([60]);
+});
+
+test("modifyArrayOfNums should return an array containing the sum of the 2 numbers and the last number unchanged", () => {
+    expect(modifyArrayOfNums([58, 2, 30])).toStrictEqual([60, 30]);
+});
+
+test("modifyArrayOfNums should return an array containing the sums of pairs of numbers", () => {
+    expect(modifyArrayOfNums([12, 250, 5, 34, 5624, 689])).toStrictEqual([262, 39, 6313])
+});
+
+test("modifyArrayOfNums should return an array containing the sums of pairs of numbers and the last number unchanged", () => {
+    expect(modifyArrayOfNums([12, 250, 5, 34, 5624, 689, 1])).toStrictEqual([262, 39, 6313, 1])
+});
+
+test("modifyArrayOfNums should return an array containing the sums of pairs of numbers and the last number unchanged", () => {
+    expect(modifyArrayOfNums([12, -250, -5, 34, -5624, -689, -4])).toStrictEqual([-238, 29, -6313, -4])
+});
